6823634330abb94cbb5f8f9a6dc1970373a3ae30
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-07-23 14:51:34 +0700

    Create GridView Categories.

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/components/category_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (item) => CategoryItem(
+                title: item.title,
+                color: item.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
